55c118ead557da67e62e40d7120dbeeabb79fb7f
Bkiller <newbashrabiu@gmail.com>
2025-06-29 15:14:18 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+# NOTE: Kada ka saka wannan token a GitHub
+BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"  # Sai ka saka daga Termux ko secrets env
+
+# IDs
+CHANNEL_ID = -1002741693835
+GROUP_ID = -1002700955947
